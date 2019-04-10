75cf0326c1e9b8f4be5194a3179f0536a4ad6529
qilei123 <qileimail123@gmail.com>
2019-04-09 23:50:10 -0400

    fda

--- beginner_source/finetuning_torchvision_models_tutorial.py
+++ beginner_source/finetuning_torchvision_models_tutorial.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Finetuning Torchvision Models
 =============================
@@ -21,6 +22,7 @@ Finetuning Torchvision Models
 # 
 # In this document we will perform two types of transfer learning:
 # finetuning and feature extraction. In **finetuning**, we start with a
+# pretrained model and update *all* of the model’s parameters for our new
 # task, in essence retraining the whole model. In **feature extraction**,
 # we start with a pretrained model and only update the final layer weights
 # from which we derive predictions. It is called feature extraction
