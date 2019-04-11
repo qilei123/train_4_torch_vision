4d7417c94cbdfd7100349d87af1ff96736ef4eee
qilei123 <qileimail123@gmail.com>
2019-04-11 11:58:20 -0400

    fda

--- beginner_source/finetuning_torchvision_models_tutorial.1.py
+++ beginner_source/finetuning_torchvision_models_tutorial.1.py
@@ -528,7 +528,7 @@ image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transf
 dataloaders_dict = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size, shuffle=True, num_workers=4) for x in ['train_binary', 'val_binary']}
 
 # Detect if we have a GPU available
-device = torch.device("cuda:3")# if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:0")# if torch.cuda.is_available() else "cpu")
 
 
 ######################################################################
