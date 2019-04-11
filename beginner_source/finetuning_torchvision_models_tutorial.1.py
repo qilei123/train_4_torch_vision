f1f5864c725ddefe088105515145e494f25e6312
qilei123 <qileimail123@gmail.com>
2019-04-11 12:02:34 -0400

    fda

--- beginner_source/finetuning_torchvision_models_tutorial.1.py
+++ beginner_source/finetuning_torchvision_models_tutorial.1.py
@@ -100,7 +100,7 @@ model_name = "inception"
 num_classes = 2
 
 # Batch size for training (change depending on how much memory you have)
-batch_size = 64
+batch_size = 32
 
 # Number of epochs to train for 
 num_epochs = 15
