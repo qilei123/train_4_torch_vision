7385a601df43cd2c02acece1734f89b5555ffe04
qilei123 <qileimail123@gmail.com>
2019-04-18 12:35:30 -0400

    fda

--- beginner_source/FocalLoss.py
+++ beginner_source/FocalLoss.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -19,8 +21,6 @@ class FocalLoss(nn.Module):
             size_average(bool): By default, the losses are averaged over observations for each minibatch.
                                 However, if the field size_average is set to False, the losses are
                                 instead summed for each minibatch.
-
-
     """
     def __init__(self, class_num, alpha=None, gamma=2, size_average=True):
         super(FocalLoss, self).__init__()
