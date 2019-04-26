b2ff50fa9653b971f7b6d1ad112c5f3a2ffcb285
qilei123 <qileimail123@gmail.com>
2019-04-26 16:12:06 -0400

    fda

--- beginner_source/finetuning_torchvision_models_tutorial_DR_2stages_distribute_multigpu.py
+++ beginner_source/finetuning_torchvision_models_tutorial_DR_2stages_distribute_multigpu.py
@@ -92,12 +92,12 @@ print("Torchvision Version: ",torchvision.__version__)
 
 # Top level data directory. Here we assume the format of the directory conforms 
 #   to the ImageFolder structure
-data_dir = "/data0/qilei_chen/AI_EYE/kaggle_data/dataset_2stages_balance"
+data_dir = "/data0/qilei_chen/AI_EYE/kaggle_data/dataset_2stages"
 
 # Models to choose from [resnet, alexnet, vgg, squeezenet, densenet, inception]
 model_name = "inception"
 
-model_folder_dir = '/data0/qilei_chen/AI_EYE/kaggle_data/dataset_2stages_models'
+model_folder_dir = data_dir+'/models'
 
 if not os.path.exists(model_folder_dir):
     os.makedirs(model_folder_dir)
