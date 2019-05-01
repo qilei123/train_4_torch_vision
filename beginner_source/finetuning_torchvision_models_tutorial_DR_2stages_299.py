9abacb48bb3feb7cabfdf3fb1ad573573705b47b
qilei123 <qileimail123@gmail.com>
2019-05-01 14:59:16 -0400

    fda

--- beginner_source/finetuning_torchvision_models_tutorial_DR_2stages_299.py
+++ beginner_source/finetuning_torchvision_models_tutorial_DR_2stages_299.py
@@ -49,7 +49,7 @@ import torch.optim as optim
 import numpy as np
 import sys
 from FocalLoss import FocalLoss
-sys.path.insert(0,'/data0/qilei_chen/pytorch_vision_4_DR')
+sys.path.insert(0,'/data0/qilei_chen/vision')
 import torchvision
 from torchvision import datasets, models, transforms
 import matplotlib.pyplot as plt
