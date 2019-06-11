1dc895a6272354ee5ec32344dd3f75bd4f1100e3
qilei123 <qileimail123@gmail.com>
2019-06-11 14:24:06 -0400

    fda

--- beginner_source/inference_api.py
+++ beginner_source/inference_api.py
@@ -92,6 +92,7 @@ for image_file_dir in image_file_dirs:
         print(label)
         wrong_count+=1
         cv2.imshow('test',cv2.imread(image_file_dir))
+        cv2.waitKey(0)
     count += 1
 print(1-float(wrong_count)/float(count))
 '''
