9e75086595999e16b12ee3415557ff644a3b97e8
qilei123 <qileimail123@gmail.com>
2019-06-19 19:28:07 -0400

    fda

--- beginner_source/finetuning_torchvision_models_tutorial_DR_5stages_1000_inceptionv4.py
+++ beginner_source/finetuning_torchvision_models_tutorial_DR_5stages_1000_inceptionv4.py
@@ -203,12 +203,16 @@ def initialize_model(model_name, num_classes, feature_extract, use_pretrained=Tr
         """
         model_ft = models.inceptionv4(pretrained=use_pretrained)
         set_parameter_requires_grad(model_ft, feature_extract)
+        '''
         # Handle the auxilary net
         num_ftrs = model_ft.AuxLogits.fc.in_features
         model_ft.AuxLogits.fc = nn.Linear(num_ftrs, num_classes)
         # Handle the primary net
         num_ftrs = model_ft.fc.in_features
         model_ft.fc = nn.Linear(num_ftrs,num_classes)
+        '''
+        num_ftrs = model_ft.classif.in_features
+        model_ft.classif = nn.Linear(num_ftrs,num_classes)
         input_size = input_size_
     else:
         print("Invalid model name, exiting...")
