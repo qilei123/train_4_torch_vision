84550e356c606f93fa36453eb0d8d78a63978be6
qilei123 <qileimail123@gmail.com>
2020-03-09 14:09:47 -0400

    fda

---
+++ train_gi_4.sh
@@ -0,0 +1,7 @@
+python beginner_source/GI/finetuning_gi_4.py -m "squeezenet"
+python beginner_source/GI/finetuning_gi_4.py -m "resnet"
+python beginner_source/GI/finetuning_gi_4.py -m "alexnet"
+python beginner_source/GI/finetuning_gi_4.py -m "vgg"
+python beginner_source/GI/finetuning_gi_4.py -m "densenet"
+python beginner_source/GI/finetuning_gi_4.py -m "inception"
+
