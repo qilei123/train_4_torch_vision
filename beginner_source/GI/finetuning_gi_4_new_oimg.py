afbad7ef850c66ffd234923275a322264c918efa
qilei123 <qileimail123@gmail.com>
2020-05-05 14:19:26 -0400

    fda

--- beginner_source/GI/finetuning_gi_4_new_oimg.py
+++ beginner_source/GI/finetuning_gi_4_new_oimg.py
@@ -474,7 +474,7 @@ def initialize_model(model_name, num_classes, feature_extract, use_pretrained=Tr
         input_size = 224   
 
     elif model_name == "resnext101_32x8d":
-        model_ft = models.resnext50_32x4d()
+        model_ft = models.resnext50_32x4d(pretrained=use_pretrained)
         set_parameter_requires_grad(model_ft, feature_extract)
         num_ftrs = model_ft.fc.in_features
         model_ft.fc = nn.Linear(num_ftrs, num_classes)
