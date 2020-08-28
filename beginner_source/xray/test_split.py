6dd9f188095e454c6c46ff4271f4688d05878bd1
qilei123 <qileimail123@gmail.com>
2020-08-28 16:59:48 -0400

    fda

--- beginner_source/xray/test_split.py
+++ beginner_source/xray/test_split.py
@@ -42,6 +42,7 @@ def split_set_presencia_hallazgos_tb(csv, nsplit = 4):
             temp_file_name.append(fn)
             temp_label.append(lb)
             command = "cp /data2/qilei_chen/DATA/xray/xray_images/"+fn+" /data2/qilei_chen/DATA/xray/labeled_4categories_images/"+lb
+            print(command)
             os.system(command)
     file_name = np.array(temp_file_name)
     label = np.array(temp_label)
