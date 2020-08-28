af1158471157ae72ab559130e6ef7bb7c3e80d4d
qilei123 <qileimail123@gmail.com>
2020-08-28 16:58:28 -0400

    fda

--- beginner_source/xray/test_split.py
+++ beginner_source/xray/test_split.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from  sklearn.model_selection import KFold
+import os
 LABEL_MAP = ['N','A']
 
 def split_set(csv, nsplit = 4):
@@ -24,7 +25,41 @@ def split_set(csv, nsplit = 4):
     for train_index, test_index in kf.split(file_name):
         yield file_name[train_index], label[train_index],file_name[test_index], label[test_index]
 
-if __name__ == "__main__":
+LABEL_MAP = ['CA','LI','AI','OT']
+def split_set_presencia_hallazgos_tb(csv, nsplit = 4):
+    pd_frame = pd.read_csv(csv, sep=',')
+    file_name = pd_frame.filename.to_numpy()
+    label = pd_frame.presencia_hallazgos_tb.to_numpy()
+    # remove NaN rows
+    keep = label == label
+    file_name = file_name[keep]
+    label = label[keep]
+    temp_file_name = []
+    temp_label = []
 
-    for i,(train_file_names,train_labels,test_file_names,test_labels) in enumerate(split_set("/data2/qilei_chen/DATA/xray/xray_dataset_annotations.csv")):
-        print(train_labels)    
+    for lb,fn in zip(label,file_name):
+        if lb in LABEL_MAP:
+            temp_file_name.append(fn)
+            temp_label.append(lb)
+            command = "cp /data2/qilei_chen/DATA/xray/xray_images/"+fn+" /data2/qilei_chen/DATA/xray/labeled_4categories_images/"+lb
+            os.system(command)
+    file_name = np.array(temp_file_name)
+    label = np.array(temp_label)
+    # convert string label to numeric label
+    assert len(np.unique(label)) == len(LABEL_MAP)
+    for i, l in enumerate(LABEL_MAP):
+        label[label == l] = i
+    label = label.astype(np.int)
+    unique, counts = np.unique(label, return_counts=True)
+    print(np.asarray((unique, counts)).T)
+    # build 4 fold
+    # random seed for reproducibility
+    kf = KFold(n_splits=nsplit, shuffle=True, random_state=20)
+    for train_index, test_index in kf.split(file_name):
+        yield file_name[train_index], label[train_index],file_name[test_index], label[test_index]
+
+
+if __name__ == "__main__":
+    split_set_presencia_hallazgos_tb("/data2/qilei_chen/DATA/xray/xray_dataset_annotations.csv")
+    #for i,(train_file_names,train_labels,test_file_names,test_labels) in enumerate(split_set("/data2/qilei_chen/DATA/xray/xray_dataset_annotations.csv")):
+    #    print(train_labels)    
--- video_preprocessing.py
+++ video_preprocessing.py
@@ -5,7 +5,7 @@ def video_preprocessing_trim(src_folder_dir,dst_folder_dir,roi,video_suffix = "a
     if not os.path.exists(dst_folder_dir):
         os.makedirs(dst_folder_dir)
 
-    video_file_dirs = glob.glob(os.path.join(src_folder_dir,"2019*"))
+    video_file_dirs = glob.glob(os.path.join(src_folder_dir,"*"+video_suffix))
     
     for video_file_dir in video_file_dirs:
         video_file_name = os.path.basename(video_file_dir)
@@ -15,7 +15,7 @@ def video_preprocessing_trim(src_folder_dir,dst_folder_dir,roi,video_suffix = "a
         fps = src_video.get(cv2.CAP_PROP_FPS)
         frame_size = (int(roi[2]-roi[0]), int(roi[3]-roi[1]))
         show_result_video_dir = os.path.join(dst_folder_dir,video_file_name)
-        dst_video = cv2.VideoWriter(show_result_video_dir,cv2.VideoWriter_fourcc("P", "I", "M", "1"),fps,frame_size)
+        dst_video = cv2.VideoWriter(show_result_video_dir,cv2.VideoWriter_fourcc(*'DIVX'),fps,frame_size)
         
         success,src_frame = src_video.read()
 
@@ -29,7 +29,7 @@ dst_folder_dir="/data2/qilei_chen/jianjiwanzhengshipin2/preprocessed/"
 roi = [441, 1, 1278, 720]
 video_preprocessing_trim(src_folder_dir,dst_folder_dir,roi)
 '''
-src_folder_dir="/data2/qilei_chen/jianjiwanzhengshipin2/changjing20/"
-dst_folder_dir="/data2/qilei_chen/jianjiwanzhengshipin2/preprocessed_changjing20/"
+src_folder_dir="/home/cql/Downloads/issue_videos/original_videos/"
+dst_folder_dir="/home/cql/Downloads/issue_videos/processed_videos/"
 roi = [156, 40, 698, 527]
-video_preprocessing_trim(src_folder_dir,dst_folder_dir,roi)
+video_preprocessing_trim(src_folder_dir,dst_folder_dir,roi,'avi')
