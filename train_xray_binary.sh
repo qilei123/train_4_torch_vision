9eab75820ac54034c524d3d67cd6daba1c810ff8
qilei123 <qileimail123@gmail.com>
2020-09-01 23:58:06 -0400

    fda

--- train_xray_binary.sh
+++ train_xray_binary.sh
@@ -1,7 +1,7 @@
-#python beginner_source/xray/finetuning_xray_binary.py -m "squeezenet1_0"
-#python beginner_source/xray/finetuning_xray_binary.py -m "shufflenetv2_x0_5"
-#python beginner_source/xray/finetuning_xray_binary.py -m "mobilenet_v2"
+python beginner_source/xray/finetuning_xray_binary.py -m "squeezenet1_0"
+python beginner_source/xray/finetuning_xray_binary.py -m "shufflenetv2_x0_5"
+python beginner_source/xray/finetuning_xray_binary.py -m "mobilenet_v2"
 
-python beginner_source/xray/finetuning_xray_4categories.py -m "squeezenet1_0"
-python beginner_source/xray/finetuning_xray_4categories.py -m "shufflenetv2_x0_5"
-python beginner_source/xray/finetuning_xray_4categories.py -m "mobilenet_v2"
+#python beginner_source/xray/finetuning_xray_4categories.py -m "squeezenet1_0"
+#python beginner_source/xray/finetuning_xray_4categories.py -m "shufflenetv2_x0_5"
+#python beginner_source/xray/finetuning_xray_4categories.py -m "mobilenet_v2"
