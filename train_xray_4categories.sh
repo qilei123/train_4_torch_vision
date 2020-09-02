751cafedd8f92de2cc1f22b4648b6d63589da37e
qilei123 <qileimail123@gmail.com>
2020-09-02 00:14:13 -0400

    fda

---
+++ train_xray_4categories.sh
@@ -0,0 +1,3 @@
+python beginner_source/xray/finetuning_xray_4categories.py -m "squeezenet1_0"
+python beginner_source/xray/finetuning_xray_4categories.py -m "shufflenetv2_x0_5"
+python beginner_source/xray/finetuning_xray_4categories.py -m "mobilenet_v2"
