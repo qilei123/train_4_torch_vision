49f46681d7612fd97ef65774985ac13b1ed074d0
qilei123 <qileimail123@gmail.com>
2020-11-25 22:29:29 -0500

    fda

--- beginner_source/DR/finetuning_natural_retina.py
+++ beginner_source/DR/finetuning_natural_retina.py
@@ -33,7 +33,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description='model name')
 parser.add_argument('--model', '-m', help='set the training model', default="alexnet")
-parser.add_argument('--datadir', '-d', help='set the training dataset', default="/data1/qilei_chen/DATA/DB_NATURAL")
+parser.add_argument('--datadir', '-d', help='set the training dataset', default="/data1/qilei_chen/DATA/DB_NATURAL/data1")
 args = parser.parse_args()
 
 
