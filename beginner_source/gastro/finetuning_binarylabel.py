117061ae35625f0921e4fcf898a18d36f95b36ee
qilei123 <qileimail123@gmail.com>
2020-12-07 17:01:22 -0500

    fda

--- beginner_source/gastro/finetuning_binarylabel.py
+++ beginner_source/gastro/finetuning_binarylabel.py
@@ -676,7 +676,14 @@ def initialize_model(model_name, num_classes, feature_extract, use_pretrained=Tr
         num_ftrs = model_ft.fc.in_features
         model_ft.fc = nn.Linear(num_ftrs,num_classes)
         input_size = input_size_
-
+    elif model_name == "mobilenetv2":
+        """ Densenet
+        """
+        model_ft = models.MobileNetV2(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.classifier[1].in_features
+        model_ft.classifier[1] = nn.Linear(num_ftrs,num_classes)
+        input_size = 224
     else:
         print("Invalid model name, exiting...")
         exit()
